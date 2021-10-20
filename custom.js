0fa6a0d8fcfe9f51a224bfe90645a893e1606b83
Sarah Hui <sarah.a.hui@gmail.com>
2021-10-20 14:11:57 +1300

    Use js click events for button handlers

--- custom.js
+++ custom.js
@@ -1,23 +1,28 @@
-let root = document.documentElement;
-let imageContainer = document.getElementById("moving-around");
+(function() {
+  let root = document.documentElement;
+  let imageContainer = document.getElementById("moving-around");
 
-imageContainer.addEventListener("mousemove", e => {
-  console.log(e);
-  root.style.setProperty('--mouse-x', e.pageX - imageContainer.offsetLeft + "px");
-  root.style.setProperty('--mouse-y', e.pageY - imageContainer.offsetTop + "px");
-});
+  let twinkBtn = document.getElementById("twink-button");
+  let bearBtn = document.getElementById("bear-button");
+  let jockBtn = document.getElementById("jock-button");
 
-function showTwink() {
-  var element = document.getElementById("twink-container");
-  element.classList.toggle("sm-twink-images-container--show");
-}
+  imageContainer.addEventListener("mousemove", e => {
+    root.style.setProperty('--mouse-x', e.pageX - imageContainer.offsetLeft + "px");
+    root.style.setProperty('--mouse-y', e.pageY - imageContainer.offsetTop + "px");
+  });
 
-function showBear() {
-  var element = document.getElementById("bear-container");
-  element.classList.toggle("sm-bear-images-container--show");
-}
+  twinkBtn.addEventListener("click", e => {
+    var element = document.getElementById("twink-container");
+    element.classList.toggle("sm-twink-images-container--show");
+  });
 
-function showJock() {
-  var element = document.getElementById("jock-container");
-  element.classList.toggle("sm-jock-images-container--show");
-}
+  bearBtn.addEventListener("click", e => {
+    var element = document.getElementById("bear-container");
+    element.classList.toggle("sm-bear-images-container--show");
+  });
+
+  jockBtn.addEventListener("click", e => {
+    var element = document.getElementById("jock-container");
+    element.classList.toggle("sm-jock-images-container--show");
+  });
+})();
