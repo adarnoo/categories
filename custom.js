9c940de66f359e63366ecda54c109f31d532c53d
Sarah Hui <sarah.a.hui@gmail.com>
2021-10-20 14:07:36 +1300

    Move all script to custom.js

--- custom.js
+++ custom.js
@@ -6,3 +6,18 @@ imageContainer.addEventListener("mousemove", e => {
   root.style.setProperty('--mouse-x', e.pageX - imageContainer.offsetLeft + "px");
   root.style.setProperty('--mouse-y', e.pageY - imageContainer.offsetTop + "px");
 });
+
+function showTwink() {
+  var element = document.getElementById("twink-container");
+  element.classList.toggle("sm-twink-images-container--show");
+}
+
+function showBear() {
+  var element = document.getElementById("bear-container");
+  element.classList.toggle("sm-bear-images-container--show");
+}
+
+function showJock() {
+  var element = document.getElementById("jock-container");
+  element.classList.toggle("sm-jock-images-container--show");
+}
