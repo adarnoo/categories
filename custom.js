8d019cd561a614fb4839c158d349360f85bd2b7a
adarnoo <91919799+adarnoo@users.noreply.github.com>
2021-10-20 17:29:38 +1300

    final edits to composition

--- custom.js
+++ custom.js
@@ -5,10 +5,12 @@
   let twinkBtn = document.getElementById("twink-button");
   let bearBtn = document.getElementById("bear-button");
   let jockBtn = document.getElementById("jock-button");
+  let linksBtn = document.getElementById("links-button");
 
   var twinkContainer = document.getElementById("twink-container");
   var bearContainer = document.getElementById("bear-container");
   var jockContainer = document.getElementById("jock-container");
+  var linksContainer = document.getElementById("links-container");
 
   let twinkInnerContainer = twinkContainer.getElementsByClassName('js-moving-around')[0];
   let bearInnerContainer = bearContainer.getElementsByClassName('js-moving-around')[0];
@@ -24,6 +26,9 @@
   jockBtn.addEventListener("click", e => {
     jockContainer.classList.add("sm-jock-images-container--show");
   });
+  linksBtn.addEventListener("click", e => {
+    linksContainer.classList.add("sm-links-images-container--show");
+  });
 
   // Remove images showing when u click on image layers
   twinkContainer.addEventListener("click", e => {
@@ -34,6 +39,9 @@
   });
   jockContainer.addEventListener("click", e => {
     jockContainer.classList.remove("sm-jock-images-container--show");
+  });
+  linksContainer.addEventListener("click", e => {
+    linksContainer.classList.remove("sm-links-images-container--show");
   });
 
   function movingAround (container, e) {
@@ -49,6 +57,9 @@
     movingAround(bearInnerContainer, e);
   });
   jockInnerContainer.addEventListener("mousemove", e => {
+    movingAround(jockInnerContainer, e);
+  });
+  linksInnerContainer.addEventListener("mousemove", e => {
     movingAround(jockInnerContainer, e);
   });
 
